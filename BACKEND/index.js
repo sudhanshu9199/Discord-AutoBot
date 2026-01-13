607ec4c9137cab73ea71031fb98bc6a898155544
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2026-01-13 23:47:24 +0530

    first commit

---
+++ BACKEND/index.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+client.once('ready', () => {
+    console.log('Bot is ready!');
+})
+
+client.login('Your_BOT_TOKEN');
